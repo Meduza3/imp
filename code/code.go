0d6f044bc2ee57bbc7887c611e90c35d14a23789
Meduza3 <jellyfish200325@gmail.com>
2025-01-09 01:32:21 +0100

    Refactor opcode definitions to use string literals and simplify operand handling in instruction creation

--- code/code.go
+++ code/code.go
@@ -1,115 +1,54 @@
 package code
 
-import (
-	"encoding/binary"
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-type Instructions []byte
-
-type Opcode byte
+type Opcode = string
 
 const (
-	GET Opcode = iota
-	PUT
-	LOAD
-	STORE
-	LOADI
-	STOREI
-	ADD
-	SUB
-	ADDI
-	SUBI
-	SET
-	HALF
-	JUMP
-	JPOS
-	JZERO
-	JNEG
-	RTRN
-	HALT
-	OpConstant
+	GET    Opcode = "GET"
+	PUT           = "PUT"
+	LOAD          = "LOAD"
+	STORE         = "STORE"
+	LOADI         = "LOADI"
+	STOREI        = "STOREI"
+	ADD           = "ADD"
+	SUB           = "SUB"
+	ADDI          = "ADDI"
+	SUBI          = "SUBI"
+	SET           = "SET"
+	HALF          = "HALF"
+	JUMP          = "JUMP"
+	JPOS          = "JPOS"
+	JZERO         = "JZERO"
+	JNEG          = "JNEG"
+	RTRN          = "RTRN"
+	HALT          = "HALT"
 )
 
 type Definition struct {
-	Name          string
-	OperandWidths []int // Number of bytes each operand uses up.
+	Name        string
+	NumOperands int
 }
 
-// All instruction definitions matching the C++ VM
 var definitions = map[Opcode]*Definition{
-	GET:    {"GET", []int{8}},    // 8 bytes for long long address
-	PUT:    {"PUT", []int{8}},    // 8 bytes for long long address
-	LOAD:   {"LOAD", []int{8}},   // 8 bytes for long long address
-	STORE:  {"STORE", []int{8}},  // 8 bytes for long long address
-	LOADI:  {"LOADI", []int{8}},  // 8 bytes for long long address
-	STOREI: {"STOREI", []int{8}}, // 8 bytes for long long address
-	ADD:    {"ADD", []int{8}},    // 8 bytes for long long address
-	SUB:    {"SUB", []int{8}},    // 8 bytes for long long address
-	ADDI:   {"ADDI", []int{8}},   // 8 bytes for long long address
-	SUBI:   {"SUBI", []int{8}},   // 8 bytes for long long address
-	SET:    {"SET", []int{8}},    // 8 bytes for long long value
-	HALF:   {"HALF", []int{8}},   // 8 bytes for long long address
-	JUMP:   {"JUMP", []int{8}},   // 8 bytes for long long offset
-	JPOS:   {"JPOS", []int{8}},   // 8 bytes for long long offset
-	JZERO:  {"JZERO", []int{8}},  // 8 bytes for long long offset
-	JNEG:   {"JNEG", []int{8}},   // 8 bytes for long long offset
-	RTRN:   {"RTRN", []int{8}},   // 8 bytes for long long address
-	HALT:   {"HALT", []int{}},    // No operands
-}
-
-func (ins Instructions) String() string {
-	var out strings.Builder
-
-	i := 0
-	for i < len(ins) {
-		def, err := Lookup(ins[i])
-		if err != nil {
-			fmt.Fprintf(&out, "ERROR: %s\n", err)
-			continue
-		}
-
-		operands, read := ReadOperands(def, ins[i+1:])
-		fmt.Fprintf(&out, "%04d $s\n", i, ins.formatInstruction(def, operands))
-
-		i += 1 + read
-	}
-
-	return out.String()
-}
-
-func (ins Instructions) formatInstruction(def *Definition, operands []int64) string {
-	operandCount := len(def.OperandWidths)
-
-	if len(operands) != operandCount {
-		return fmt.Sprintf("ERROR: operand length %d does not match defined %d",
-			len(operands), operandCount)
-	}
-
-	switch operandCount {
-	case 0:
-		return def.Name
-	case 1:
-		return fmt.Sprintf("%s %d", def.Name, operands[0])
-	}
-
-	return fmt.Sprintf("ERROR: unhandled operand count for %s", def.Name)
-}
-
-func ReadOperands(def *Definition, ins Instructions) ([]int64, int) {
-	operands := make([]int64, len(def.OperandWidths))
-	offset := 0
-
-	for i, width := range def.OperandWidths {
-		switch width {
-		case 8:
-			operands[i] = int64(binary.BigEndian.Uint64(ins[offset:]))
-			offset += 8
-		}
-	}
-
-	return operands, offset
+	GET:    {"GET", 1},
+	PUT:    {"PUT", 1},
+	LOAD:   {"LOAD", 1},
+	STORE:  {"STORE", 1},
+	LOADI:  {"LOADI", 1},
+	STOREI: {"STOREI", 1},
+	ADD:    {"ADD", 1},
+	SUB:    {"SUB", 1},
+	ADDI:   {"ADDI", 1},
+	SUBI:   {"SUBI", 1},
+	SET:    {"SET", 1},
+	HALF:   {"HALF", 0},
+	JUMP:   {"JUMP", 1},
+	JPOS:   {"JPOS", 1},
+	JZERO:  {"JZERO", 1},
+	JNEG:   {"JNEG", 1},
+	RTRN:   {"RTRN", 1},
+	HALT:   {"HALT", 0},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -120,32 +59,30 @@ func Lookup(op byte) (*Definition, error) {
 	return def, nil
 }
 
-func Make(op Opcode, operands ...int64) ([]byte, error) {
+type Instruction struct {
+	Opcode  Opcode
+	Operand int64
+}
+
+func Make(op Opcode, operands ...int64) (*Instruction, error) {
 	def, ok := definitions[op]
 	if !ok {
-		return nil, fmt.Errorf("opcode %d undefined", op)
+		return nil, fmt.Errorf("code %s not defined", op)
 	}
-
-	instructionLen := 1
-	for _, w := range def.OperandWidths {
-		instructionLen += w
-	}
-
-	instruction := make([]byte, instructionLen)
-	instruction[0] = byte(op)
-
-	offset := 1
-	for i, operand := range operands {
-		if i >= len(def.OperandWidths) {
-			return nil, fmt.Errorf("too many operands provided for %s", def.Name)
+	switch len(operands) {
+	case 0:
+		if def.NumOperands == 0 {
+			return &Instruction{Opcode: op}, nil
+		} else {
+			return nil, fmt.Errorf("missing operand for opcode %s", op)
 		}
-		width := def.OperandWidths[i]
-
-		switch width {
-		case 8:
-			binary.BigEndian.PutUint64(instruction[offset:], uint64(operand))
+	case 1:
+		if def.NumOperands == 1 {
+			return &Instruction{Opcode: op, Operand: operands[0]}, nil
+		} else {
+			return nil, fmt.Errorf("unnecesary operand for opcode %s", op)
 		}
-		offset += width
+	default:
+		return nil, fmt.Errorf("only 0 or 1 operands")
 	}
-	return instruction, nil
 }
--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,35 +1,77 @@
 package compiler
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/Meduza3/imp/ast"
 	"github.com/Meduza3/imp/code"
-	"github.com/Meduza3/imp/object"
 )
 
 type Compiler struct {
-	instructions code.Instructions
-	constants    []object.Object
+	instructions []code.Instruction
 }
 
 func New() *Compiler {
 	return &Compiler{
-		instructions: code.Instructions{},
-		constants:    []object.Object{},
+		instructions: []code.Instruction{},
 	}
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, p := range node.Procedures {
+			err := c.Compile(&p)
+			if err != nil {
+				return err
+			}
+		}
+		err := c.Compile(&node.Main)
+		if err != nil {
+			return err
+		}
+	case *ast.Main:
+		// for _, decl := range node.Declarations {
+		// 	err := c.compileDeclaration(&decl)
+		// 	if err != nil {
+		// 		return err
+		// 	}
+		// }
+		for _, comm := range node.Commands {
+			err := c.Compile(comm)
+			if err != nil {
+				return err
+			}
+		}
+	case *ast.WriteCommand:
+		int, _ := strconv.Atoi(node.Value.String()) //later add support for identifier values
+		c.emit(code.PUT, int64(int))
+	}
 	return nil
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int64) (int, error) {
+	ins, err := code.Make(op, operands...)
+	if err != nil {
+		return 0, fmt.Errorf("failed to make instruction for %v(%v): %v", op, operands, err)
+	}
+	pos := c.addInstruction(*ins)
+	return pos, nil
+}
+
+func (c *Compiler) addInstruction(ins code.Instruction) int {
+	posNewInstructon := len(c.instructions)
+	c.instructions = append(c.instructions, ins)
+	return posNewInstructon
 }
 
 func (c *Compiler) Bytecode() *Bytecode {
 	return &Bytecode{
 		Instructions: c.instructions,
-		Constants:    c.constants,
 	}
 }
 
 type Bytecode struct {
-	Instructions code.Instructions
-	Constants    []object.Object
+	Instructions []code.Instruction
 }
